c0d0ad3d6bc537c7b37082250f9d0c8d8fd81535
olga-alexandrovnaa <42916312+olga-alexandrovnaa@users.noreply.github.com>
2021-04-04 15:27:42 +0300

    Create class Arcanoid

---
+++ Breakout/arcanoid.h
@@ -0,0 +1,46 @@
+#ifndef ARCANOID_H
+#define ARCANOID_H
+
+
+#include <QWidget>
+#include <QKeyEvent>
+#include "ball.h"
+#include "brick.h"
+#include "platform.h"
+
+
+class Arcanoid {
+public:
+    Arcanoid(QWidget* parent = 0);
+    ~Arcanoid();
+
+protected:
+    void paintEvent(QPaintEvent *);
+    void timerEvent(QTimerEvent *);
+    void keyPressEvent(QKeyEvent *);
+    void keyReleaseEvent(QKeyEvent *);
+    void drawObjects(QPainter *);
+    void finishGame(QPainter *, QString);
+    void moveObjects();
+    void startGame();
+    void pauseGame();
+    void stopGame();
+    void victory();
+    void checkCollision();
+
+private:
+    int x;
+    int timerId;
+    static const int N_OF_BRICKS = 30;
+    static const int DELAY = 10;
+    static const int BOTTOM_EDGE = 400;
+    Ball* ball;
+    Platform* paddle;
+    Brick* bricks[N_OF_BRICKS];
+    bool gameOver;
+    bool gameWon;
+    bool gameStarted;
+    bool paused;
+};
+
+#endif // ARCANOID_H
